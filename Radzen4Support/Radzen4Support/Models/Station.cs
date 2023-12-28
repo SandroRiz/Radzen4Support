3f9fc88eff2c3c108128edbd869070e8dfe7e4b8
Sandro Rizzetto <sandro.rizzetto@microgate.it>
2023-12-28 16:35:24 +0100

    config

---
+++ Radzen4Support/Radzen4Support/Models/Station.cs
@@ -0,0 +1,24 @@
+﻿namespace Radzen4Support.Models;
+
+public class WeatherStation
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public decimal Altitude { get; set; }
+
+    public decimal Latitude { get; set; }
+
+    public decimal Longitude { get; set; }
+
+    public int StationCategoryId { get; set; }
+
+    public string StationTypeId { get; set; } = null!;
+
+    public bool IsOfficial { get; set; }
+
+    public bool IsOnline { get; set; }
+
+    public int? CivisId { get; set; }
+}
